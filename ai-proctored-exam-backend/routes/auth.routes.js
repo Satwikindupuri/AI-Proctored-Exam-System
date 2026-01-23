da63856875b15059f6750594c41d321fb8d5e2f2
Satwik Indupuri <satwikindupuri1432@gmail.com>
2026-01-23 20:45:17 +0530

    Delete ai-proctored-exam-backend directory

--- ai-proctored-exam-backend/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, createTestUsers } = require("../controllers/auth.controller");
-
-router.post("/login", login);
-// router.post("/seed", createTestUsers); // TEMP route
-
-module.exports = router;
